57982f138087570cc872ad5b156102abc1e36714
Shivendra Pratap Jadia <shivendrapratapjadia@gmail.com>
2024-06-19 13:34:06 +0530

    made change to AppEntry.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import RangeSelectionScreen from "./screens/RangeSelectionScreen";
-import HomeScreen from "./screens/HomeScreen";
-import { DateRangeProvider } from "./DateRangerContext";
+import RangeSelectionScreen from "./src/screens/RangeSelectionScreen";
+import HomeScreen from "./src/screens/HomeScreen";
+import { DateRangeProvider } from "./src/DateRangerContext";
 
 const Stack = createNativeStackNavigator();
 
